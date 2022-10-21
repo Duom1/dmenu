9ab3a1859d208b3c7d4ea984911379f21579a6ea
Your Name <you@example.com>
2022-10-21 13:28:08 +0300

    hehehehe

---
+++ colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#afbbcc", "#080F37" },
+	[SchemeSel] = { "#afbbcc", "#2F4775" },
+	[SchemeOut] = { "#afbbcc", "#007CC3" },
+};
